9bf99cc9ae549f3b6356575a733ced63b2fc2b66
Steven Smail <41452253+ssmail7@users.noreply.github.com>
2021-02-13 20:43:16 -0500

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -1,4 +1,4 @@
-//***************************************************************
+//**************************************************************
 // Written by Steven Smail for COP3331
 //
 // Employee.h (Project 2)
@@ -6,21 +6,44 @@
 // This is the header containing the Employee class and member
 // function definitions - including Constructors, Accessor
 // functions, and Mutator functions.
-//***************************************************************
+//**************************************************************
 
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
-using namespace std;// Employee class declarationclass Employee{  private:    string empName;    int monthlySalary;  public:
-    // Constructors    Employee()    { empName = " ";
+using namespace std;
+
+// Employee class declaration
+class Employee
+{
+  private:
+    string empName;
+    int monthlySalary;
+  public:
+    // Constructors
+    Employee()
+    { empName = " ";
       monthlySalary = 0; }
 
-    Employee(string name, int salary)    { empName = name;
+    Employee(string name, int salary)
+    { empName = name;
       monthlySalary = salary; }
 
-    // Accessor functions    string getEmpName() const    { return empName; }    int getMonthlySalary() const    { return monthlySalary; }
+    // Accessor functions
+    string getEmpName() const
+    { return empName; }
 
-    int getYearlySalary() const    { return monthlySalary * 12; }
+    int getMonthlySalary() const
+    { return monthlySalary; }
 
-    // Mutator functions    void setEmpName(string name)    { empName = name; }    void setMonthlySalary(int salary)    { monthlySalary = salary; }
+    int getYearlySalary() const
+    { return monthlySalary * 12; }
+
+    // Mutator functions
+    void setEmpName(string name)
+    { empName = name; }
+
+    void setMonthlySalary(int salary)
+    { monthlySalary = salary; }
 };
-#endif
+
+#endif
